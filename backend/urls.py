5ed55859f05cb470ff7f9f12f7f8548118121146
ljj1410446243 <1410446243@qq.com>
2025-07-12 19:11:20 +0800

    测试pdf文件的读取

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from material.views import material_list
+from material.views import material_list, upload_material
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # 把 core 的 URL 合并进来
-    path('materials/', material_list),
+    path('materials/', material_list, name='material_list'),
+    path('upload/', upload_material, name='upload'),
 ]
+
+# 显示上传的文件
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- material/models.py
+++ material/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # 上传素材表（测试）
 class UploadedMaterial(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
     file = models.FileField(upload_to='materials/')
     file_type = models.CharField(max_length=20)
     upload_time = models.DateTimeField(auto_now_add=True)
+    extracted_text = models.TextField(blank=True)  # 用于存储提取出的文本
 
     def __str__(self):
         return self.title
