c5b5ee1bc5bc6ff24f03ea614d3b4a25cb85c855
ljj1410446243 <1410446243@qq.com>
2025-07-23 23:08:17 +0800

    完成答题情况统计后端逻辑和接口，接口测试完成

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import generate_ai_quiz, quiz_list, publish_quiz, close_quiz
+from .views import generate_ai_quiz, quiz_list, publish_quiz, close_quiz, quiz_statistics
 from django.http import JsonResponse
 
 def test_view(request):
@@ -11,4 +11,5 @@ urlpatterns = [
     path('list/<int:presentation_id>/', quiz_list, name='quiz_list'),
     path('publish/<int:quiz_id>/', publish_quiz, name='publish_quiz'),
     path('close/<int:quiz_id>/', close_quiz, name='close_quiz'),
+    path('stats/<int:presentation_id>/', quiz_statistics, name='quiz_statistics'),
 ]
